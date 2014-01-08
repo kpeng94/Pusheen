68598930c3ef207bdbfd6907fdead16f3f86920d
Obscuron <Abscuron@gmail.com>
2014-01-08 17:31:14 -0500

    updated generic player

--- teams/genericplayer/RobotPlayer.java
+++ teams/genericplayer/RobotPlayer.java
@@ -3,13 +3,26 @@ package genericplayer;
 import battlecode.common.*;
 
 public class RobotPlayer {
-	static Rand rand;
 	static final Direction[] dir = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
 	
-	public static void run(RobotController rc) {
+	/* Static values (these do not change over multiple rounds) */
+	static Rand rand; // Random number generator
+	static RobotController rc; // Robot Controller
+	
+	/* Dynamic values (these change every round, but are cached to prevent recomputation */
+	static MapLocation curLoc; // Current location of the robot
+	
+	/* Main Method */
+	public static void run(RobotController rcin) {
+		/* Initialize static values */
 		rand = new Rand(rc.getRobot().getID());
+		rc = rcin;
 		
 		while (true) {
+			/* Save dynamic values */
+			curLoc = rc.getLocation();
+			
+			/* Distributes actions to their respective functions */
 			switch(rc.getType()) {
 			case HQ:
 				try {
@@ -28,19 +41,43 @@ public class RobotPlayer {
 				}
 				break;
 			case PASTR:
+				try {
+					runPastr();
+				}
+				catch (Exception e) {
+					System.out.println("Pastr Exception");
+				}
 				break;
 			case NOISETOWER:
+				try {
+					runNoise();
+				}
+				catch (Exception e) {
+					System.out.println("NoiseTower Exception");
+				}
 				break;
 			}
-			rc.yield();
+			rc.yield(); // Yields to save remaining bytecodes
 		}
 	}
 
-	private static void runHQ() {
+	/* Runs code for HQ */
+	private static void runHQ() throws GameActionException {
 			
 	}
 	
-	private static void runSoldier() {
+	/* Runs code for Soldiers */
+	private static void runSoldier() throws GameActionException {
+		
+	}
+
+	/* Runs code for PASTRS */
+	private static void runPastr() throws GameActionException {
+		
+	}
+
+	/* RUns code for Noise Towers */
+	private static void runNoise() throws GameActionException {
 		
 	}
 	
