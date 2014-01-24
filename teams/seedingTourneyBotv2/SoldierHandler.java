a02e1502a7cbdcd536e43600f9966de2b546041f
Kevin Peng <kpeng94@mit.edu>
2014-01-23 20:59:01 -0500

    not 10 rounds

--- teams/seedingTourneyBotv2/SoldierHandler.java
+++ teams/seedingTourneyBotv2/SoldierHandler.java
@@ -154,7 +154,7 @@ public class SoldierHandler extends UnitHandler {
 		if (enemyPASTRs.length == 0) {
 			bumRushing = false;
 		}
-		if (enemyPASTRs.length >= 2 || (Clock.getRoundNum() % 10 == 0 && rc.senseTeamMilkQuantity(rc.getTeam().opponent()) > 
+		if (enemyPASTRs.length >= 2 || (rc.senseTeamMilkQuantity(rc.getTeam().opponent()) > 
 		 rc.senseTeamMilkQuantity(rc.getTeam()) && enemyPASTRs.length >= 1)) {	
 			// Attack the PASTR that is farthest from the enemy.
 			// Perhaps write some code later that will avoid HQs altogether.
