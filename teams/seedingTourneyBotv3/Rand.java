969d4f22037e83f93cb1ea75b9b2778de29c3a54
Xubo Sun <xubosun@mit.edu>
2014-01-28 01:07:02 -0500

    attack micro: simple swarm move incorporated

---
+++ teams/seedingTourneyBotv3/Rand.java
@@ -0,0 +1,36 @@
+package seedingTourneyBotv3;
+
+/**
+ * 
+ * @author Rene
+ * 
+ * Class for handling random integer generation
+ * Initialize with a seed using the constructor
+ * 
+ * next() returns a random integer
+ * next(int max) returns a nonnegative integer less than max
+ * nextAnd(int n) returns a nonnegative integer bitwise anded with n
+ *
+ */
+public class Rand {
+	static final long a = 0xffffda61L;
+	private long x;
+	
+	public Rand(long seed) {
+		x = seed & 0xffffffffL;
+	}
+	
+	public int next() {
+		x = (a * (x & 0xffffffffL)) + (x >>> 32);
+		return (int) x;
+	}
+	
+	public int next(int max) {
+		return (next() & 0x7fffffff) % max;
+	}
+	
+	public int nextAnd(int n) {
+		return next() & n;
+	}
+	
+}
