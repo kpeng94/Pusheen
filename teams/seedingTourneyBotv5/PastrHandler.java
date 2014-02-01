81b449b61c1aed45e8c4465176c11f09dfbd676a
Xubo Sun <xubosun@mit.edu>
2014-02-01 03:01:35 -0500

    retreat incorporated;lots of exceptions

---
+++ teams/seedingTourneyBotv5/PastrHandler.java
@@ -0,0 +1,17 @@
+package seedingTourneyBotv5;
+
+import battlecode.common.*;
+
+public class PastrHandler extends UnitHandler {
+
+	public PastrHandler(RobotController rcin) {
+		super(rcin);
+	}
+
+	@Override
+	public void execute() throws GameActionException{
+		super.execute();
+		rc.broadcast(15000, Clock.getRoundNum());
+	}
+
+}
